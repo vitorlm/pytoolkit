f1a1252f842eeea3828cc7763eee6cf0225b919c
Vitor Mendonça <vitorlm@gmail.com>
2025-01-23 19:14:01 -0300

    Updating the method to summarize the year.

--- src/domains/syngenta/team_assessment/core/team_summary.py
+++ src/domains/syngenta/team_assessment/core/team_summary.py
@@ -43,30 +43,45 @@ class TeamSummary:
             return
 
         summary_aggregations = {
-            "total_tasks": 0,
-            "total_planned_duration": 0,
-            "total_actual_duration": 0,
+            "total_work_days": 0,
+            "total_epics": 0,
+            "total_non_epics": 0,
+            "total_planned_days": 0,
+            "total_executed_days": 0,
             "average_adherence_to_dates": 0,
+            "adherence_categories": {},
             "average_efficiency": 0,
             "average_resource_utilization": 0,
-            "time_spent_bug": 0,
-            "time_spent_spillover": 0,
-            "time_spent_out": 0,
-            "total_cycle_days": 0,
+            "epics_by_type": {},
+            "members_contribution": {},
+            "total_members": 0,
+            "total_bug_days": 0,
+            "total_spillover_days": 0,
+            "total_off_days": 0,
         }
 
         adherence_categories = {"Excellent": 0, "Good": 0, "Fair": 0, "Poor": 0}
-        tasks_by_type = {}
+        epics_by_type = {}
         members_contribution = {}
 
+        unique_non_epics = set()
         for cycle in self._cycles.values():
             summary = cycle.summary
-            summary_aggregations["total_cycle_days"] += cycle.cycle_work_days
-            summary_aggregations["total_tasks"] += summary.get("total_tasks", 0)
-            summary_aggregations["total_planned_duration"] += summary.get(
-                "total_planned_duration", 0
-            )
-            summary_aggregations["total_actual_duration"] += summary.get("total_actual_duration", 0)
+
+            summary_aggregations["total_work_days"] += cycle.cycle_work_days
+            summary_aggregations["total_epics"] += summary.get("total_epics", 0)
+
+            for epic in cycle.backlog.values():
+                if (
+                    epic
+                    and epic.code.lower() in self._config.epics_helper_codes
+                    and epic.code.lower() not in unique_non_epics
+                ):
+                    unique_non_epics.add(epic.code.lower())
+                    summary_aggregations["total_non_epics"] += len(unique_non_epics)
+
+            summary_aggregations["planned_cycle_days"] += summary.get("planned_cycle_days", 0)
+            summary_aggregations["executed_cycle_days"] += summary.get("executed_cycle_days", 0)
             summary_aggregations["average_adherence_to_dates"] += summary.get(
                 "average_adherence_to_dates", 0
             )
@@ -83,47 +98,48 @@ class TeamSummary:
                 adherence_categories[category] += count
 
             for task_type, count in summary.get("tasks_by_type", {}).items():
-                tasks_by_type[task_type] = tasks_by_type.get(task_type, 0) + count
+                epics_by_type[task_type] = epics_by_type.get(task_type, 0) + count
 
             for member, contribution in summary.get("members_contribution", {}).items():
                 if member not in members_contribution:
-                    members_contribution[member] = {"planned_tasks": 0, "executed_tasks": 0}
-                members_contribution[member]["planned_tasks"] += contribution.get(
-                    "planned_tasks", 0
-                )
-                members_contribution[member]["executed_tasks"] += contribution.get(
-                    "executed_tasks", 0
+                    members_contribution[member] = {"planned_item": 0, "executed_item": 0}
+                members_contribution[member]["planned_item"] += contribution.get("planned_item", 0)
+                members_contribution[member]["executed_item"] += contribution.get(
+                    "executed_item", 0
                 )
 
-        average_tasks_per_cycle = (
-            summary_aggregations["total_tasks"] / self._cycle_count if self._cycle_count else 0
+        average_epics_per_cycle = (
+            summary_aggregations["total_epics"] / self._cycle_count if self._cycle_count else 0
         )
 
-        average_time_spent_bugs_per_cycle = (
-            summary_aggregations["time_spent_bug"] / self._cycle_count if self._cycle_count else 0
+        average_bugs_days_per_cycle = (
+            summary_aggregations["total_bug_days"] / self._cycle_count if self._cycle_count else 0
         )
 
-        average_time_spent_spillover_per_cycle = (
-            summary_aggregations["time_spent_spillover"] / self._cycle_count
+        average_spillover_per_cycle = (
+            summary_aggregations["total_spillover_days"] / self._cycle_count
             if self._cycle_count
             else 0
         )
 
-        average_time_spent_out_per_cycle = (
-            summary_aggregations["time_spent_out"] / self._cycle_count if self._cycle_count else 0
+        average_off_days_per_cycle = (
+            summary_aggregations["total_off_days"] / self._cycle_count if self._cycle_count else 0
         )
 
-        total_working_days = (
-            summary_aggregations["total_cycle_days"] - summary_aggregations["time_spent_out"]
+        effective_availability = (
+            summary_aggregations["total_work_days"] - summary_aggregations["total_off_days"]
+        )
+
+        effective_work_days = (
+            summary_aggregations["total_work_days"]
+            - summary_aggregations["total_spillover_days"]
+            - summary_aggregations["total_bug_days"]
+            - summary_aggregations["total_off_days"]
         )
 
         efficiency = (
-            (
-                summary_aggregations["total_actual_duration"]
-                / summary_aggregations["total_cycle_days"]
-            )
-            * 100
-            if summary_aggregations["total_cycle_days"]
+            (summary_aggregations["total_executed_days"] / effective_work_days) * 100
+            if effective_work_days
             else 0
         )
         bug_ratio = (
@@ -201,7 +217,7 @@ class TeamSummary:
             "average_efficiency": summary_aggregations["average_efficiency"] / self._cycle_count,
             "average_resource_utilization": summary_aggregations["average_resource_utilization"]
             / self._cycle_count,
-            "tasks_by_type": tasks_by_type,
+            "tasks_by_type": epics_by_type,
             "members_contribution": members_contribution,
             "total_members": len(self._member_list),
             "total_time_spent_bug": summary_aggregations["time_spent_bug"],
